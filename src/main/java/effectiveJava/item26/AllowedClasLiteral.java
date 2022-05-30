fd474b5cb6ab3cd960a44d180cbffd2dcbb430f4
ynoolee <heerarea129@gmail.com>
2022-05-31 02:15:59 +0900

    feat : effectiveJava item26

---
+++ src/main/java/effectiveJava/item26/AllowedClasLiteral.java
@@ -0,0 +1,27 @@
+package effectiveJava.item26;
+
+import java.util.List;
+import java.util.Set;
+
+public class AllowedClasLiteral {
+	void testClassLiteral(){
+		Class<List> listClass = List.class;
+		// List<String>.class 불가능  - Class literal 에는 Parameterized type 을 사용할 수 없다.
+		Class<String[]> aClass = String[].class; // 배열에 대해서는 허용
+		Class<Integer> integerClass = int.class; // 원시타입에 대해서도 허용
+	}
+
+	void useInstanceOfToGenericType(Object set) {
+		Set<?> converted = (Set<?>)set;
+
+		if ( set instanceof Set) {
+			Set<?> mySet = (Set<?>)set;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		AllowedClasLiteral main = new AllowedClasLiteral();
+		main.useInstanceOfToGenericType(Set.of("a","b"));
+	}
+}
---
+++ src/main/java/effectiveJava/item26/WildCard.java
@@ -0,0 +1,34 @@
+package effectiveJava.item26;
+
+import java.util.Set;
+
+public class WildCard {
+
+	public void getWildcardCollection(Set<?> collection){
+		// read 는 정상적으로 수행된다
+		for (Object o : collection) {
+			System.out.println(o);
+		}
+
+		collection.add(null);	// null 외에는 추가할 수 없다
+		// collection.add(Integer.valueOf(10)); // 컴파일 에러
+
+	}
+	public void getCollection(Set collection){
+		// read 는 정상적으로 수행된다
+		for (Object o : collection) {
+			System.out.println(o);
+		}
+	}
+
+	public static void main(String[] args) {
+		WildCard wildCard = new WildCard();
+
+		Set<?> set = Set.of("a",1);
+
+
+		wildCard.getCollection(set);
+		wildCard.getWildcardCollection(set);
+
+	}
+}
