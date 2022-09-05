9f90dfddbae8f103eb961c42219d1cfcf94f11e2
ynoolee <heerarea129@gmail.com>
2022-09-05 14:20:01 +0900

    feat : Item 58 관련 Iterator, index 테스트 추가
    - 배열을 기반으로 하는 ArrayList 의 경우 iterator 를 사용하나 index 를 사용하나 별 차이가 없는 모습을 볼 수 있음
    - LinkedList 의 경우 index 를 사용한 접근 시, 실질적으로는 메모리 상에 sequential 하게 저장되어 있는 것이 아니다 보니, 매 번 O(n) 의 복잡도를 가지게 됨. 그러다보니 엄청난 시간이 걸린다는 것을 측정할 수 있음. 반면 Iterator 를 사용하면 빠른 접근이 가능함.

--- src/test/java/book/effectiveJava/item58/AdvancedForTest.java
+++ src/test/java/book/effectiveJava/item58/AdvancedForTest.java
@@ -2,6 +2,9 @@ package book.effectiveJava.item58;
 
 import book.effectiveJava.item58.AdvancedFor.OUTER;
 import book.effectiveJava.item58.AdvancedFor.Pair;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.IntStream;
 import org.assertj.core.api.Assertions;
@@ -41,6 +44,109 @@ class AdvancedForTest {
             ints = IntStream.range(0,100000000).toArray();
             sum = 0;
         }
+        @Test
+        @DisplayName("index 로 전체 원소들을 Iterate 하는데 걸리는 시간")
+        void iterateV2() {
+            for (int idx = 0; idx < ints.length; idx++) {
+                sum += ints[idx];
+            }
+        }
+
+        @Test
+        @DisplayName("for each 로 전체 원소들을 iterate 하는데 걸리는 시간")
+        void iterateV1() {
+            for (int numb : ints) {
+                sum += numb;
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("List 에 대해 iterator 를 사용하는 경우와 index 를 사용하는 경우")
+    class ListTest{
+        private List<Integer> list;
+        private int size = 100000;
+        private int sum;
+
+        @BeforeEach
+        void setUp(){
+            sum = 0;
+        }
+
+        @Nested
+        @DisplayName("ArrayList 에 대해 iterator 를 사용하는 경우와 index 를 사용하는 경우")
+        class ArrayListTest {
+
+            @BeforeEach
+            void setUp() {
+                list = new ArrayList<>();
+
+                IntStream.range(0,size)
+                    .forEach(numb -> list.add(numb));
+            }
+
+            @Test
+            @DisplayName("index 를 사용하여 Iterate 하는 경우 시간 측정")
+            void iterateByIndex() {
+                for(int idx = 0; idx < size; idx++) {
+                    sum += list.get(idx);
+                }
+            }
+
+            @Test
+            @DisplayName("iterator 를 사용하여 iterate 하는 경우 시간 측정")
+            void iterateByIterator() {
+                for (Integer numb : list) {
+                    sum += numb;
+                }
+            }
+        }
+
+        @Nested
+        @DisplayName("LinkedList 에 대해 iterator 를 사용하는 경우와 index 를 사용하는 경우")
+        class LinkedListTest {
+            @BeforeEach
+            void setUp() {
+                list = new LinkedList<>();
+
+                IntStream.range(0,size)
+                    .forEach(numb -> list.add(numb));
+            }
+
+            @Test
+            @DisplayName("index 를 사용하여 Iterate 하는 경우 시간 측정")
+            void iterateByIndex() {
+                for(int idx = 0; idx < size; idx++) {
+                    sum += list.get(idx);
+                }
+            }
+
+            @Test
+            @DisplayName("iterator 를 사용하여 iterate 하는 경우 시간 측정")
+            void iterateByIterator() {
+                for (Integer numb : list) {
+                    sum += numb;
+                }
+            }
+        }
+    }
+
+
+
+
+    @Nested
+    @DisplayName("Wrapper 타입에 대해 index 접근 및 for each 접근 속도 테스트")
+    class WrapperTest {
+        private Integer[] ints;
+        private int sum;
+
+        @BeforeEach
+        void setUp() {
+            ints = new Integer[100000000];
+            Arrays.fill(ints, Integer.valueOf(1));
+            sum = 0;
+        }
+
         @Test
         @DisplayName("index 로 전체 원소들을 Iterate 하는데 걸리는 시간")
         void iterateV2() {
