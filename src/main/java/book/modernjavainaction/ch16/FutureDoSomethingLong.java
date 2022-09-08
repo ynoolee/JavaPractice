12cc2cbe1805a11ca154c135c9148386f5faf39a
ynoolee <heerarea129@gmail.com>
2022-09-08 23:31:43 +0900

    feat : CompletableFuture 를 사용해 메인스레드 블록킹 문제 해결
    - CompletableFuture 에서는 Future 와 달리, 기존 스레드를 블록킹 하지 않고도 Future 결과로 원하는 동작을 수행할 수 있다
    - 하지만 여기서 역시 executorService.awaitTermination() 을 호출하여 또 다시 blocking 하고 있기는 하다. 그래도 기존에 Future 결과를 얻어와서 추가적인 작업을 하기 위한 블록킹을 하던 부분은 사라질 수 있음을 확인할 수 있다.

--- src/main/java/book/modernjavainaction/ch16/FutureDoSomethingLong.java
+++ src/main/java/book/modernjavainaction/ch16/FutureDoSomethingLong.java
@@ -1,11 +1,9 @@
 package book.modernjavainaction.ch16;
 
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 public class FutureDoSomethingLong {
 
@@ -20,24 +18,24 @@ public class FutureDoSomethingLong {
             "[" + Thread.currentThread().getName() + "]" + " Complete to generate thread pool : "
                 + (System.nanoTime() - start));
 
-        Future<Double> future = executorService.submit(this::runSomethingLong);
-        doSomething();
-        try {
-            Double result = future.get(1,
-                TimeUnit.SECONDS); // 현재 스레드가 대기할 최대 타임아웃 시간 설정 -> 타임아웃시 TimeOutException 발생
-
-            System.out.println(
+        CompletableFuture.supplyAsync(this::runSomethingLong)
+            .thenAccept(numb -> System.out.println(
                 "[" + Thread.currentThread().getName() + "]" + " FUTURE result received : "
-                    + result);
-        } catch (ExecutionException ee) {
-        
-        } catch (InterruptedException ie) {
-            // 현재 스레드의 대기 중에 interrupt 발생
-        } catch (TimeoutException te) {
-            te.printStackTrace(); // Future 완료 전 타임 아웃 발생 : f
+                    + numb));
+
+        doSomething();
+
+        try {
+            if (!executorService.awaitTermination(3, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+
+                Thread.getAllStackTraces().keySet()
+                    .forEach(thread -> System.out.println(thread.getName()));
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
         }
 
-        executorService.shutdown();
 
     }
 
