f95ebe18b5817d9b383046d69300a83f5fca6e49
ynoolee <heerarea129@gmail.com>
2022-11-17 17:00:50 +0900

    feat : Item 88 을 위해 Item 50 의 Period 객체 정의와 동일한 Period 추가

---
+++ src/main/java/book/effectiveJava/ch12/item88/Period.java
@@ -0,0 +1,72 @@
+package book.effectiveJava.ch12.item88;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+// Broken "immutable" time period class (Pages 231-3)
+public final class Period implements Serializable {
+
+    private final Date start;
+
+    private final Date end;
+
+//    /**
+//     * @param  start the beginning of the period
+//     * @param  end the end of the period; must not precede start
+//     * @throws IllegalArgumentException if start is after end
+//     * @throws NullPointerException if start or end is null
+//     */
+//    public Period(Date start, Date end) {
+//        if (start.compareTo(end) > 0)
+//            throw new IllegalArgumentException(
+//                start + " after " + end);
+//        this.start = start;
+//        this.end   = end;
+//    }
+//
+//    public Date start() {
+//        return start;
+//    }
+//    public Date end() {
+//        return end;
+//    }
+
+    public String toString() {
+        return start + " - " + end;
+    }
+
+    // Repaired constructor - makes defensive copies of parameters (Page 232)
+    public Period(Date start, Date end) {
+        this.start = new Date(start.getTime());
+        this.end = new Date(end.getTime());
+
+        if (this.start.compareTo(this.end) > 0) {
+            throw new IllegalArgumentException(
+                this.start + " after " + this.end);
+        }
+    }
+
+    // Repaired accessors - make defensive copies of internal fields (Page 233)
+    public Date start() {
+        return new Date(start.getTime());
+    }
+
+    public Date end() {
+        return new Date(end.getTime());
+    }
+
+    // Remainder omitted
+
+    @Serial
+    private void readObject(ObjectInputStream s) throws IOException, ClassNotFoundException {
+        s.defaultReadObject();
+
+        if (this.start.compareTo(this.end) > 0) {
+            throw new IllegalArgumentException(
+                this.start + " after " + this.end);
+        }
+    }
+}
