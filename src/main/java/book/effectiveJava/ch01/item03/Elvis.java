54646218001d516e304accbd45e2a32a21eb77d0
ynoolee <heerarea129@gmail.com>
2023-01-24 19:14:42 +0900

    feat : item 3 - Elvis 싱글톤 객체 만들기 ver 2 가 깨지는 경우 - 직렬화

--- src/main/java/book/effectiveJava/ch01/item03/Elvis.java
+++ src/main/java/book/effectiveJava/ch01/item03/Elvis.java
@@ -1,16 +1,18 @@
 package book.effectiveJava.ch01.item03;
 
-public class Elvis {
+import java.io.Serializable;
 
-    private static final Elvis elvis = new Elvis();
+public class Elvis implements Serializable {
+
+    public static final Elvis INSTANCE = new Elvis();
 
     private static boolean created;
 
     private Elvis() {
-        if (created) {
-            throw new UnsupportedOperationException("can't be created again by cosntructor");
-        }
-
-        created = true;
+//        if (created) {
+//            throw new UnsupportedOperationException("can't be created again by cosntructor");
+//        }
+//
+//        created = true;
     }
 }
