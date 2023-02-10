38bdda62c51ec8d6b0217c29ed99d96af827388b
ynoolee <heerarea129@gmail.com>
2023-02-10 14:59:48 +0900

    feat: Reactive Stream 의 기본 복습 - 가장 기초적인 Publisher, Subscriber 작성

---
+++ src/main/java/learningJava/reactive/streams/Operator.java
@@ -0,0 +1,79 @@
+package learningJava.reactive.streams;
+
+import java.util.Iterator;
+import java.util.concurrent.Flow.Publisher;
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+// Publisher 의 역할 : data stream 을 계속해서 만들어내는 Provider 역할
+// Mono 와 Flux 역시 그런 역할인건가?
+
+public class Operator {
+
+    public static void main(String[] args) {
+
+        Publisher<Integer> pub = new Publisher<Integer>() {
+            // Publisher 가 publish 할 src 라고 생각
+            Iterable<Integer> iter = Stream.iterate(1, a -> a + 1).limit(10)
+                .collect(Collectors.toList());
+
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) {
+                // subscribe 가 호출되면 그 때 부터 데이터를 보내야 한다.
+                // subscriber 의 구독이 일어날 때 마다 이 과정이 일어난다.
+                sub.onSubscribe(new Subscription() {
+
+                    @Override
+                    public void request(long n) {
+                        try {
+                            // n 무시하고 다 넘기는 거로 해 보자
+                            iter.forEach(s -> sub.onNext(s));
+                            // ( 사실 publisher 측에서는,데이터를 하나 날리고 나면 이 구독이 cancle 되지는 않았는지도 체크 해 봐야 함 )
+                            // publisher 측에서는 자신이 가진 데이터를 모두 publish 했다 하면, 반드시 onComplete, onError 를 해야 한다
+                            sub.onComplete();
+                        } catch (Throwable t) {
+                            sub.onError(t);
+                        }
+                    }
+
+                    @Override
+                    public void cancel() {
+                        // subscriber 측에서 완료가 아님에도 데이터를 더이상 받고 싶지 않을 때 cancel 을 날릴 수도 있다.
+                    }
+                });
+            }
+        };
+
+        Subscriber<Integer> sub = new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                System.out.println(String.format("onSubscribe:"));
+                // Subscription 이 넘어오면 가장 먼저 해야 할 일 : request 를 해야 한다.
+                // Subscriber 에게 Subscription 이 전달된 것 = 구독 채널이 만들어진 것.
+                subscription.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer item) {
+                System.out.println(String.format("onNext:"));
+            }
+
+            // Publisher 측에서는 예외가 발생하더라도 예외를 throw 하는게 아니라, 반드시 Subscriber 의 onError 메소드를 통해 Throwable 타입의 객체로 넘겨줘야 한다. (우아하게~)
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println(String.format("onError:"));
+
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println(String.format("onComplete:"));
+
+            }
+        };
+
+        pub.subscribe(sub);
+    }
+}
